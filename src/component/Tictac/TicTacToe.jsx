14f9ee1ba7acd323621d044b1fdde46ca966ebca
hamzarahmoun <hamza.rahmoun456@gmail.com>
2024-10-01 08:34:38 +0100

    design done

--- src/component/Tictac/TicTacToe.jsx
+++ src/component/Tictac/TicTacToe.jsx
@@ -7,6 +7,21 @@ const TicTacToe = () => {
     <div className='container'>
        <h className="title"> Tic Tac Toe Game In <span>React</span></h>
        <div className="board">
+        <div className="row1">
+            <div className="boxes"></div>
+            <div className="boxes"></div>
+            <div className="boxes"></div>
+        </div>
+        <div className="row2">
+            <div className="boxes"></div>
+            <div className="boxes"></div>
+            <div className="boxes"></div>
+        </div>
+        <div className="row3">
+            <div className="boxes"></div>
+            <div className="boxes"></div>
+            <div className="boxes"></div>
+        </div>
 
        </div>
        <button className="reset">
